b17b851efd1d56fd9914cfc9a76fb5d31c676925
Sept <dleb42@gmail.com>
2014-05-09 15:34:51 -0500

    Added rest of types, assigned booleans

--- src/main/java/engine/EnumTypes.java
+++ src/main/java/engine/EnumTypes.java
@@ -4,7 +4,12 @@ package src.main.java.engine;
  * Created by 618616 on 5/9/2014.
  */
 public enum EnumTypes {
-    FIRE(),
-    WATER(),
+    FIRE(false, false, false, false, false, false, false),
+    WATER(false, false, false, false, false, false, false),
+    ELEC(false, false, false, false, false, false, false),
+    GRASS(false, false, false, false, false, false, false),
+    FLYING(false, false, false, false, false, false, false),
+    PSYCHIC(false, false, false, false, false, false, false),
+    GHOST(false, false, false, false, false, false, false),
 
 }
