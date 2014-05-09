27b36ca870172021f85048fa0c3296743b125644
HotelCalifornia <ak.brooke@sbcglobal.net>
2014-05-08 20:45:32 -0500

    changed the Menu class to accept a type parameter (Menu<E>)

--- src/main/java/engine/Menu.java
+++ src/main/java/engine/Menu.java
@@ -1,10 +1,26 @@
 package src.main.java.engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class sets up the basic properties of a menu
  */
-public class Menu {
-    public Menu() {
+public class Menu<E> {
 
+    //stores the options available in this menu
+    private List<E> options;
+    public List<E> getOptions() { return options; }
+
+    public Menu() {
+        options = new ArrayList<E>();
+    }
+
+    //display the contents of the menu
+    public void displayMenu() {
+        int i = 0;
+        for(E element : options) {
+            System.out.println(i+element.toString());
+        }
     }
 }
