5c6aa81265d3ac3c20cba51e834c986063152027
HotelCalifornia <ak.brooke@sbcglobal.net>
2014-05-08 20:23:22 -0500

    added beginning of the game 'engine' (heh) and the menu parent class

---
+++ src/main/java/engine/Engine.java
@@ -0,0 +1,10 @@
+package src.main.java.engine;
+
+/**
+ * This class sets up all of the components required for running the game.
+ */
+public class Engine {
+    public Engine() {
+
+    }
+}
---
+++ src/main/java/engine/Menu.java
@@ -0,0 +1,10 @@
+package src.main.java.engine;
+
+/**
+ * This class sets up the basic properties of a menu
+ */
+public class Menu {
+    public Menu() {
+
+    }
+}
