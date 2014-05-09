b2a32632f554b44fdffec6f10670d3744486cead
HotelCalifornia <ak.brooke@sbcglobal.net>
2014-05-09 15:34:25 -0500

    did some stuff

---
+++ src/main/java/engine/AbstractCreature.java
@@ -0,0 +1,12 @@
+package src.main.java.engine;
+
+/**
+ * Created by 618616 on 5/9/2014.
+ */
+public abstract class AbstractCreature {
+    //the creature's type
+    private EnumTypes type;
+    public EnumTypes getType() { return type; }
+
+
+}
