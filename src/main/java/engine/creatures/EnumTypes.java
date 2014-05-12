dc7bf2881969563b931ec46d8e5a14b490659ecb
Alex (a.k.a kintama.tar.gz) <ak.brooke@sbcglobal.net>
2014-05-11 20:49:08 -0500

    example
    added an explanation of the int values and how they translate to type effectiveness

--- src/main/java/engine/creatures/EnumTypes.java
+++ src/main/java/engine/creatures/EnumTypes.java
@@ -26,13 +26,21 @@ public enum EnumTypes {
 
     }
     static int isEffective(EnumTypes type1, EnumTypes type2) {
-        if     (type1.fire    && type2  ==    FIRE) { return 0; }
-        else if(type1.water   && type2  ==   WATER) { return 0; }
-        else if(type1.elec    && type2  ==    ELEC) { return 0; }
-        else if(type1.grass   && type2  ==   GRASS) { return 0; }
-        else if(type1.flying  && type2  ==  FLYING) { return 0; }
-        else if(type1.psychic && type2  == PSYCHIC) { return 0; }
-        else if(type1.ghost   && type2  ==   GHOST) { return 0; }
+        /**
+         * each int stores the relative effectiveness of the type on another type.
+         * so, if type.fire == 1, it's supereffective against fire
+         * type.fire == 0 means there's normal efffectiveness (no multiplier)
+         * and type.fire == -1 means it deals half damage against fire (not very effective)
+         * so, this method should make the appropriate comparisons as such
+         * if(type1.fire == 1 && type2 == FIRE) { return 1; }
+         */
+        if     (type1.fire    && type2  ==    FIRE) { return 1; }
+        else if(type1.water   && type2  ==   WATER) { return 1; }
+        else if(type1.elec    && type2  ==    ELEC) { return 1; }
+        else if(type1.grass   && type2  ==   GRASS) { return 1; }
+        else if(type1.flying  && type2  ==  FLYING) { return 1; }
+        else if(type1.psychic && type2  == PSYCHIC) { return 1; }
+        else if(type1.ghost   && type2  ==   GHOST) { return 1; }
         else                                        { return 0; }
     }
 
