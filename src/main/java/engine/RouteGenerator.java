fa952cd805c2dd26019d830d5c49f94df3bae96e
Griffin <618873@eths202.org>
2014-05-15 09:59:51 -0500

    Update RouteGenerator.java
    add route length generation

--- src/main/java/engine/RouteGenerator.java
+++ src/main/java/engine/RouteGenerator.java
@@ -1,6 +1,16 @@
 //Creates route lengths
 package src.main.java.engine;
 @SuppressWarnings("unused") //stops the warnings complaining that this class is unused
-public class RouteGenerator {
-
+public class RouteGenerator 
+{
+  //defines variables
+  int routeNum = 0;
+  int playerSteps = 0;
+  boolean pokemonAppear = false;
+  int routeLength = 0;
+  
+  //determines the route length between 5 and 15
+  routeLength= 5 + (int)(Math.random()*10);
+  
+  
 }
