f907c921b613f44936cc60839c6064c091b300db
Griffin <618873@eths202.org>
2014-05-16 17:24:37 -0500

    Update RouteGenerator.java
    added route navigation, player UI, and beginnings of transfer from route to town. more flushed out version soon.

--- src/main/java/engine/RouteGenerator.java
+++ src/main/java/engine/RouteGenerator.java
@@ -1,16 +1,59 @@
 //Creates route lengths
+import java.util.*;
 package src.main.java.engine;
 @SuppressWarnings("unused") //stops the warnings complaining that this class is unused
-public class RouteGenerator 
+public static void main(String[] args)
 {
   //defines variables
   int routeNum = 0;
   int playerSteps = 0;
   boolean pokemonAppear = false;
   int routeLength = 0;
+  Scanner move = new Scanner(System.in);
+  char direction = null;
+  int i = 1;
   
-  //determines the route length between 5 and 15
-  routeLength= 5 + (int)(Math.random()*10);
-  
-  
+}
+public class RouteGenerator
+{
+    //determines the route length between 5 and 15
+    routeLength= 5 + (int)(Math.random()*10);
+    //gives info about route to player upon entering route
+    system,out.println("Route " + routeNum);
+    system.out.println("The route is " + routeLength + " spaces long");
+    //handles player movement 
+    while(i <= routeLength)
+    {
+      //gives aditional info about route and instructions about how to navigate route
+      System.out.println("you are on space " + i)
+      System.out.println("Which direction would you like to go?");
+      System.out.println("Foward:F/Backward:B");
+      //accepts player input
+      direction = move.nextLine();
+      // progresses in player chosen direction
+      if (direciton == 'f' || direction == 'F')
+      {
+        i++;
+      }
+      else if (direction == 'b' || direction == 'B')
+      {
+        i--;
+      }
+      //error catch
+      else
+      {
+        System.out.println("Error: unrecognized direction")
+      }
+      //puts player into next or previous town
+      if (i <= 0)
+      {
+        routeNum--;
+        //need town generation to complete
+      }
+      else if (i > 15)
+      {
+        routeNum++;
+        //need town generation to complete
+      }
+    }
 }
