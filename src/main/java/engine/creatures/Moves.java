99afd24f6f8295c47094ae700ad5d6f84c9f8002
Alex (a.k.a kintama.tar.gz) <ak.brooke@sbcglobal.net>
2014-05-20 15:12:04 -0500

    Update Moves.java

--- src/main/java/engine/creatures/Moves.java
+++ src/main/java/engine/creatures/Moves.java
@@ -4,11 +4,18 @@ package src.main.java.engine.creatures;
  * Created by 618616 on 5/16/2014.
  */
 public class Moves {
+    private String name;
+    public String getName() { return name; }
+    
     private int damage;
     public int getDamage() { return damage; }
 
     private EnumTypes type;
     public EnumTypes getType() { return type; }
 
-
+    public Moves(String name, int damage, EnumTypes type) {
+        this.name   =   name;
+        this.damage = damage;
+        this.type   =   type;
+    }
 }
