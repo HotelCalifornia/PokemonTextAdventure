21a982bf002ec54ad9e58cf7334e5d5b23f88e3b
Sept <dleb42@gmail.com>
2014-05-20 15:35:48 -0500

    Made Lists of names

--- src/main/java/engine/MovesList.java
+++ src/main/java/engine/MovesList.java
@@ -1,5 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import src.main.java.engine.creatures.Moves;
 public static class MovesList {
-  public static makeMoves() {
+  private ArrayList<Moves> moves = new ArrayList<Moves>;
+  public static void makeMoves() {
+    ArrayList<String> fireNames = new ArrayList<String>;
+    fireNames.add(0, "Pyro");
+    fireNames.add(1, "Nova");
+    fireNames.add(2, "Cinder");
+    fireNames.add(3, "Corona");
+    fireNames.add(4, "Fire");
+    ArrayList<String> waterNames = new ArrayList<String>;
+    waterNames.add(0, "Hydro");
+    waterNames.add(1, "Aqua");
+    waterNames.add(2, "Ocean");
+    waterNames.add(3, "Swamp");
+    waterNames.add(4, "Water");
+    ArrayList<String> grassNames = new ArrayList<String>;
+    grassNames.add(0, "Leaf");
+    grassNames.add(1, "Grass");
+    grassNames.add(2, "Vine");
+    grassNames.add(3, "Solar");
+    grassNames.add(4, "Ground");
+    ArrayList<String> elecNames = new ArrayList<String>;
+    elecNames.add(0, "Thunder");
+    elecNames.add(1, "Volt");
+    elecNames.add(2, "Lightning");
+    elecNames.add(3, "Quick");
+    elecNames.add(4, "Zap");
+    ArrayList<String> flyNames = new ArrayList<String>;
+    flyNames.add(0, "Wind");
+    flyNames.add(1, "Beak");
+    flyNames.add(2, "Wing");
+    flyNames.add(3, "Gale");
+    flyNames.add(4, "Quill");
+    ArrayList<String> psyNames = new ArrayList<String>;
+    psyNames.add(0, "Nuero");
+    psyNames.add(1, "Mind");
+    psyNames.add(2, "Psy");
+    psyNames.add(3, "Insane");
+    psyNames.add(4, "Psionic");
+    ArrayList<String> ghostNames = new ArrayList<String>;
+    ghostNames.add(0, "Ecto");
+    ghostNames.add(1, "Shadow");
+    ghostNames.add(2, "Final");
+    ghostNames.add(3, "Spectre");
+    ghostNames.add(4, "Reaper");
+    for(int i=0; i<6; i++)
+    {
+      
+    }
     
   }
 }
