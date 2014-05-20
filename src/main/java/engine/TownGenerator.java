6dc83e48c34574601cad510cd4956b31dcc2c73b
Griffin <618873@eths202.org>
2014-05-20 14:18:32 -0500

    Create TownGenerator.java
    added town generation

---
+++ src/main/java/engine/TownGenerator.java
@@ -0,0 +1,6 @@
+//Creates route lengths
+import java.util.*;
+package src.main.java.engine;
+@SuppressWarnings("unused") //stops the warnings complaining that this class is unused
+public class RouteGenerator
+{
