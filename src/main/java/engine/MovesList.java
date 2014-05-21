22a3e4f9168ccf3cea1938e1ee2e423620d0c9ea
Sept <dleb42@gmail.com>
2014-05-20 19:53:57 -0500

    Creates moves (probably)

--- src/main/java/engine/MovesList.java
+++ src/main/java/engine/MovesList.java
@@ -1,6 +1,9 @@
+import java.util.*;
+import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
 import src.main.java.engine.creatures.Moves;
+import src.main.java.engine.creatures.EnumTypes;
 public static class MovesList {
   private ArrayList<Moves> moves = new ArrayList<Moves>;
   public static void makeMoves() {
@@ -13,7 +16,7 @@ public static class MovesList {
     ArrayList<String> waterNames = new ArrayList<String>;
     waterNames.add(0, "Hydro");
     waterNames.add(1, "Aqua");
-    waterNames.add(2, "Ocean");
+    waterNames.add(2, "Frost");
     waterNames.add(3, "Swamp");
     waterNames.add(4, "Water");
     ArrayList<String> grassNames = new ArrayList<String>;
@@ -46,10 +49,55 @@ public static class MovesList {
     ghostNames.add(2, "Final");
     ghostNames.add(3, "Spectre");
     ghostNames.add(4, "Reaper");
-    for(int i=0; i<6; i++)
-    {
-      
+    ArrayList<String> suffixes = new ArrayList<String>;
+    suffixes.add(0, "Beam");
+    suffixes.add(1, "Blast");
+    suffixes.add(2, "Bolt");
+    suffixes.add(3, "Slash");
+    suffixes.add(4, "Slap");
+    suffixes.add(5, "Scratch");
+    suffixes.add(6, "Strike");
+    suffixes.add(7, "Burn");
+    suffixes.add(8, "Burst");
+    suffixes.add(9, "Squall");
+    suffixes.add(10, "Pulse");
+    suffixes.add(11, "Shock");
+    suffixes.add(12, "Discharge");
+    suffixes.add(13, "Ungentlemanly Response");
+    suffixes.add(14, "Curse");
+    ArrayList<EnumTypes> types = new ArrayList<EnumTypes>;
+    types.add(EnumTypes FIRE);
+    types.add(EnumTypes WATER);
+    types.add(EnumTypes GRASS);
+    types.add(EnumTypes ELEC);
+    types.add(EnumTypes FLYING);
+    types.add(EnumTypes PSYCHIC);
+    types.add(EnumTypes GHOST);
+
+      Random rand = new Random(7)+1;
+      EnumTypes type = types.get(rand)
+      Random ran = new Random(5)+1;
+      String pref = new String;
+      if(type==FIRE)
+        pref = fireNames.get(ran);
+      else if (type==WATER)
+        pref = waterNames.get(ran);
+      else if (type==GRASS)
+        pref = grassNames.get(ran);
+      else if (type==ELEC)
+        pref = elecNames.get(ran);
+      else if (type==FLYING)
+        pref = flyNames.get(ran);
+      else if (type==PSYCHIC)
+        pref = psyNames.get(ran);
+      else if (type==GHOST)
+        pref = ghostNames.get(ran);
+      Random ra = new Random(15)+1;
+      String suf = suffixes.get(ra);
+      String name = pref + " " + suf;
+      Moves move = new Moves(name, , type)
     }
+    
     
   }
 }
