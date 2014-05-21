560360d0d93c25fd8f256efbccff35d25d4b45da
HotelCalifornia <ak.brooke@sbcglobal.net>
2014-05-21 18:20:28 -0500

    beginning of the player class.. seems too good to be true

---
+++ src/main/java/engine/Player.java
@@ -0,0 +1,38 @@
+package src.main.java.engine;
+
+import src.main.java.engine.creatures.AbstractCreature;
+
+/**
+ * This file created by Alex Brooke
+ * please seek the author's permission before
+ * distributing this software.
+ * 'Do you know Java? Because your method body is sexy'
+ * :3
+ */
+//this is the big one :0
+public class Player {
+    private AbstractCreature[] party = new AbstractCreature[6 ];
+    private AbstractCreature[] box   = new AbstractCreature[64];
+    public Player() {
+        //is this really it?
+    }
+    public void addCreatureToPartyOrBox(AbstractCreature creature) {
+        for(int i = 0; i < party.length; i++) {
+            if(party[i] == null) {
+                party[i] = creature;
+                break;
+            }
+            if(i == party.length - 1 && party[i] != null) {
+                for(int j = 0; i < box.length; i++) {
+                    if(box[j] == null) {
+                        box[j] = creature;
+                        break;
+                    }
+                    if(j == box.length - 1 && box[j] != null) {
+                        //what happens if the box is full? :o
+                    }
+                }
+            }
+        }
+    }
+}
