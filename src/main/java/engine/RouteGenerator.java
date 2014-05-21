93d25dc77f7f744b72e6e60c252623952e9da26e
Sept <dleb42@gmail.com>
2014-05-21 15:22:12 -0500

    Added TownGenerator()

--- src/main/java/engine/RouteGenerator.java
+++ src/main/java/engine/RouteGenerator.java
@@ -49,7 +49,7 @@ public class RouteGenerator
       else if (i > routeLength)
       {
         routeNum++;
-        //need town generation to complete
+        TownGenerator();
       }
     }
   }
