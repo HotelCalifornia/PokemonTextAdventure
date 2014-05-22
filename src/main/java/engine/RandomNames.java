4f17fb6fd4287869282ab26d31a45999f1e6a52b
Sept <dleb42@gmail.com>
2014-05-21 20:41:36 -0500

    Added Lists

--- src/main/java/engine/RandomNames.java
+++ src/main/java/engine/RandomNames.java
@@ -6,6 +6,32 @@ public class RandomNames
 
     public static String getName(ArrayList<String> prefix, ArrayList<String> suffix)
     {
+        ArrayList<String> prefix = new ArrayList<String>();
+        prefix.add("Pyro");
+        prefix.add("Aqua");
+        prefix.add("Cryo");
+        prefix.add("Gaia");
+        prefix.add("Rai");
+        prefix.add("Regi");
+        prefix.add("Sora");
+        prefix.add("Tri");
+        prefix.add("Avus");
+        prefix.add("Aether");
+        prefix.add("Umbra");
+        prefix.add("Neuro");
+        prefix.add("Xeo");
+        ArrayList<String> suffix = new ArrayList<String>();
+        suffix.add("Mon");
+        suffix.add("Nova");
+        suffix.add("Rex");
+        suffix.add("Wind");
+        suffix.add("Leaf");
+        suffix.add("Razor");
+        suffix.add("Shine");
+        suffix.add("Blitz");
+        suffix.add("Hawk");
+        suffix.add("Drake");
+        suffix.add("Wave");
         Random ran = new Random();
         int prefSpot = 1+(int)(Math.random() *prefix.size());
         int sufSpot = 1+(int)(Math.random()*suffix.size());
