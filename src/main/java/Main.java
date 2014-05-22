8ad936ba3bd03b75c32018fe7895676f7aa4f76f
Griffin <618873@eths202.org>
2014-05-21 21:32:26 -0500

    potato

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,8 @@
 package src.main.java;
-
 import src.main.java.engine.ExampleMenu;
-
 import java.util.ArrayList;
 
-/**
- * Story base: you have long-term memory loss, so you can't remember your way
- * home after you leave. As such, you can never go back to a town that is more than
- * 1 route-length away. Example:
- *
- * X - - - T <- player is in the first town
- * T X - - T <- player is one route-length away from the town. they can still go back
- * T - X - T <- player is two route-lengths away from the first town, can no longer go back
- *
- * This method would allow us to only save multiple towns if there are multiple towns
- * within one route-length of the player
- */
 public class Main 
 {
+  Engine intro = new Engine();
 }
